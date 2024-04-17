2136b2e23935358e9581c14c624d076ef3d32301
Hamish Gamble <hamish.gamble@flinders.edu.au>
2024-04-17 10:20:06 +0930

    Adds functions to add product data (ie colours or names) to complexity datasets

--- R/product_data.R
+++ R/product_data.R
@@ -1,15 +1,61 @@
-#' Harmonised System (1992) product data. 
-#' 
-#' A dataset containing the names, level, and product code of products categorised under the 
-#' 1992 version of the Harmonised System
-#' 
-#' @format A dataframe with 6406 rows and 5 columns:
-#' \describe{
-#' \item{product_id}{id}
-#' \item{hs_product_code}{character representation of the product code}
-#' \item{hs_product_name_short_en}{english name of the product}
-#' \item{level}{level of detail the product is categorised under (can be section 2 digits, 4 digits, 6 digits)}
-#' \item{parent_id}{which overarching level the product fits under}
+#' Add product names to a dataset
+#'
+#' @param data a data frame with a column of hs92 product codes
+#' @param digits representation of hs92 product codes. either four or six digits. 
+#'
+#' @return tibble
+#' @export
+#'
+#' @examples \dontrun{
+#' library(complexitydata)
+#' state_economic_complexity |> 
+#' add_product_digits(digits = 4)
 #' }
-#' @source \url{https://dataverse.harvard.edu/file.xhtml?persistentId=doi:10.7910/DVN/3BAL1O/WQZPYX&version=4.0}
-"product_data"
+#' 
+add_product_names <- function(data, digits) {
+  
+  if (!digits %in% c("four", "4", 4, "six", "6", 6)) {
+    stop()
+  } else if (digits %in% c("four", "4", 4)) {
+    digits <- "4digit"
+  } else {
+    digits <- "6digit"
+  }
+  
+  prod_data <- product_data |> 
+    dplyr::filter(level == digits) |> 
+    dplyr::select(hs_product_name_short_en, hs_product_code)
+  
+  data |>  
+    dplyr::left_join(prod_data)
+}
+
+#' Add Atlas of Economic Complexity section colours to products
+#'
+#' @param data a data frame with a column of hs92 product codes
+#' @param digits representation of hs92 product codes. either four or six digits. 
+#'
+#' @return tibble
+#' @export
+#'
+#' @examples \dontrun{
+#' library(complexitydata)
+#' state_economic_complexity |> 
+#' add_product_colours(digits = 4)
+#' }
+add_product_colours <- function(data, digits) {
+  
+  if (!digits %in% c("four", "4", 4, "six", "6", 6)) {
+    stop()
+  } else if (digits %in% c("four", "4", 4)) {
+    digits <- "4digit"
+  } else {
+    digits <- "6digit"
+  }
+  
+  prod_data <- product_colours |> 
+    select(hs_product_code = hs4, colour = color)
+  
+  data |> 
+    left_join(prod_data)
+}
