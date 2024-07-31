542d3017b0db8634f1d758ad83574e2adc0a96fa
Hamish Gamble <hamish.gamble@flinders.edu.au>
2024-07-31 10:22:49 +0930

    Add graph for activity density

--- R/complexity_graphs.R
+++ R/complexity_graphs.R
@@ -81,7 +81,7 @@ graph_complexity_tree <- function(year, region) {
                   .data$location_code == {{region}},
                   .data$hs_product_code != "unspecified") |>
     dplyr::mutate(export_value_share = scales::label_percent(accuracy = 0.1, scale = 100)(.data$export_value/sum(.data$export_value)),
-           pci_label = scales::label_number(accuracy = 0.01)(.data$pci)) |> 
+                  pci_label = scales::label_number(accuracy = 0.01)(.data$pci)) |> 
     dplyr::left_join(product_data, by = "hs_product_code") |> 
     ggplot2::ggplot(ggplot2::aes(area = .data$export_value, fill = round(.data$pci, 3), subgroup = .data$section_name, 
                                  label = paste(.data$hs_product_name_short_en, .data$pci_label, sep = "\n"))) +
@@ -89,15 +89,15 @@ graph_complexity_tree <- function(year, region) {
     treemapify::geom_treemap_text(colour = "white", place = "centre", size = 15) + 
     treemapify::geom_treemap_subgroup_border(colour = "white", size = 1.5) + 
     ggplot2::scale_fill_gradientn(colours = atlas_complexity_colours()$colour, 
-                         values = atlas_complexity_colours()$percent, 
-                         breaks = c(-3.35, 2.3),
-                         labels = c("Low Complexity", "High Complexity")) +
+                                  values = atlas_complexity_colours()$percent, 
+                                  breaks = c(-3.35, 2.3),
+                                  labels = c("Low Complexity", "High Complexity")) +
     ggplot2::guides(fill = ggplot2::guide_colorbar(barwidth = 10, 
-                                 title = NULL,
-                                 direction = "horizontal",
-                                 draw.ulim = F,
-                                 draw.llim = F,
-                                 label.position = "top")) + 
+                                                   title = NULL,
+                                                   direction = "horizontal",
+                                                   draw.ulim = F,
+                                                   draw.llim = F,
+                                                   label.position = "top")) + 
     ggplot2::labs(caption = "Note: Australian product export data converted from AHECC to 4-digit Harmonised System (1992).\n
        Services export data aggregated to EBOPS - Communications, Insurance and Finance, Transportation, Travel and Other") +
     cowplot::theme_cowplot() +
@@ -106,67 +106,107 @@ graph_complexity_tree <- function(year, region) {
 }
 
 graph_complexity_product_space <- function(country, year, services = FALSE) {
-    
-    working_data <- read_complexitydata("combined_exports") |> 
-      dplyr::filter(.data$year == {{year}},
-                    !is.na(as.numeric(.data$hs_product_code)))
-    
-    mcp <- working_data |> 
-      economiccomplexity::balassa_index(discrete = TRUE,
-                                        cutoff = 1,
-                                        country = "location_code",
-                                        product = "hs_product_code",
-                                        value = "export_value")
-    
-    m <- as.matrix(mcp) |> 
-      as.data.frame() |> 
-      tibble::rownames_to_column(var = "location_code") |> 
-      tidyr::pivot_longer(cols = -.data$location_code,
-                          values_to = "m",
-                          names_to = "hs_product_code")
-    
-    prox <- economiccomplexity::proximity(mcp)
-    
-    product_space_colours <- complexity_classification
-    
-    ps_data <- working_data |> 
-      dplyr::group_by(.data$hs_product_code) |> 
-      dplyr::mutate(product_export_value = sum(.data$export_value)) |> 
-      dplyr::filter(.data$location_code == {{country}}) |> 
-      dplyr::left_join(m, by = dplyr::join_by("location_code", "hs_product_code")) |> 
-      dplyr::left_join(product_space_colours, by = dplyr::join_by("hs_product_code" == "code"))
-      dplyr::group_by(.data$colour) |> 
-      dplyr::mutate(colour_id = factor(m * dplyr::cur_group_id()), 
-                    colour = ifelse(.data$colour_id == 0, "grey", .data$colour)) |> 
-      dplyr::ungroup()
-    
-    
-    
-    graph_size <- setNames(ps_data$product_export_value, ps_data$hs_product_code)
-    graph_colour <- setNames(ps_data$colour_id, ps_data$hs_product_code)
-    
-    
-    net <- economiccomplexity::projections(prox$proximity_country, prox$proximity_product, compute = "product")
-    
-    #Adjust dots for size (this is currently location size. Could update to do global size)
-    
-    igraph::V(net$network_product)$size <- graph_size[match(igraph::V(net$network_product)$name, names(graph_size))]
-    
-    # Set attribute for "presence"
-    # igraph::V(net$network_product)$presence <- graph_presence[match(igraph::V(net$network_product)$name, names(graph_presence))] 
-    
-    # Set attribute for colour
-    igraph::V(net$network_product)$colour <- graph_colour[match(igraph::V(net$network_product)$name, names(graph_colour))]
-    
-    ggraph::ggraph(net$network_product, layout = "kk") +
-      ggraph::geom_edge_link(edge_colour = "#a8a8a8", alpha = 0.1) + 
-      ggraph::geom_node_point(ggplot2::aes(size = .data$size, colour = .data$colour)) + 
-      ggplot2::scale_colour_manual(values = setNames(ps_data$colour, ps_data$colour_id))
-    
-    
-    
-    
-  }
+  
+  working_data <- read_complexitydata("combined_exports") |> 
+    dplyr::filter(.data$year == {{year}},
+                  !is.na(as.numeric(.data$hs_product_code)))
+  
+  mcp <- working_data |> 
+    economiccomplexity::balassa_index(discrete = TRUE,
+                                      cutoff = 1,
+                                      country = "location_code",
+                                      product = "hs_product_code",
+                                      value = "export_value")
+  
+  m <- as.matrix(mcp) |> 
+    as.data.frame() |> 
+    tibble::rownames_to_column(var = "location_code") |> 
+    tidyr::pivot_longer(cols = -.data$location_code,
+                        values_to = "m",
+                        names_to = "hs_product_code")
+  
+  prox <- economiccomplexity::proximity(mcp)
+  
+  product_space_colours <- complexity_classification
+  
+  ps_data <- working_data |> 
+    dplyr::group_by(.data$hs_product_code) |> 
+    dplyr::mutate(product_export_value = sum(.data$export_value)) |> 
+    dplyr::filter(.data$location_code == {{country}}) |> 
+    dplyr::left_join(m, by = dplyr::join_by("location_code", "hs_product_code")) |> 
+    dplyr::left_join(product_space_colours, by = dplyr::join_by("hs_product_code" == "code"))
+  dplyr::group_by(.data$colour) |> 
+    dplyr::mutate(colour_id = factor(m * dplyr::cur_group_id()), 
+                  colour = ifelse(.data$colour_id == 0, "grey", .data$colour)) |> 
+    dplyr::ungroup()
+  
+  
+  
+  graph_size <- setNames(ps_data$product_export_value, ps_data$hs_product_code)
+  graph_colour <- setNames(ps_data$colour_id, ps_data$hs_product_code)
+  
+  
+  net <- economiccomplexity::projections(prox$proximity_country, prox$proximity_product, compute = "product")
+  
+  #Adjust dots for size (this is currently location size. Could update to do global size)
+  
+  igraph::V(net$network_product)$size <- graph_size[match(igraph::V(net$network_product)$name, names(graph_size))]
+  
+  # Set attribute for "presence"
+  # igraph::V(net$network_product)$presence <- graph_presence[match(igraph::V(net$network_product)$name, names(graph_presence))] 
+  
+  # Set attribute for colour
+  igraph::V(net$network_product)$colour <- graph_colour[match(igraph::V(net$network_product)$name, names(graph_colour))]
+  
+  ggraph::ggraph(net$network_product, layout = "kk") +
+    ggraph::geom_edge_link(edge_colour = "#a8a8a8", alpha = 0.1) + 
+    ggraph::geom_node_point(ggplot2::aes(size = .data$size, colour = .data$colour)) + 
+    ggplot2::scale_colour_manual(values = setNames(ps_data$colour, ps_data$colour_id))
+  
+  
+  
+  
+}
+
+#' Data Coverage
+#'
+#' @param data 
+#' @param region 
+#' @param activity 
+#' @param flip 
+#'
+#' @return ggplot2 object
+#' @export
+#'
+#' @examples \dontrun{
+#' graph_complexity_coverage(data, "sa3", "indp")
+#' }
+graph_complexity_coverage <- function(data, region, activity, flip = FALSE) {
+  
+  region_size = paste0(region, "_size")
+  activity_size = paste0(activity, "_size")
+  
+  data |> 
+    dplyr::group_by(.data[[region]])  |> 
+    dplyr::mutate({{region_size}} := sum(count)) |> 
+    dplyr::group_by(.data[[activity]]) |> 
+    dplyr::mutate({{activity_size}} := sum(count)) |> 
+    dplyr::ungroup() |> 
+    dplyr::mutate(presence = count > 0,
+                  {{region}} := forcats::fct_infreq(.data[[region]], w = .data[[region_size]]),
+                  {{activity}} := forcats::fct_infreq(.data[[activity]], w = .data[[activity_size]])) |> 
+    ggplot2::ggplot(ggplot2::aes(x = if(!flip) .data[[region]] else .data[[activity]],
+                                 y = if(!flip) .data[[activity]] else .data[[region]],
+                                 fill = presence)) + 
+    ggplot2::geom_raster() + 
+    ggplot2::scale_fill_manual(breaks = c(TRUE, FALSE),
+                               values = c("#3182db", "white")) + 
+    ggplot2::theme(axis.text = ggplot2::element_blank(),
+                   axis.ticks = ggplot2::element_blank(),
+                   panel.border = ggplot2::element_rect(fill = NA),
+                   legend.position = "none") + 
+    ggplot2::coord_equal()
+}
 
 #' @importFrom grDevices rgb
 atlas_complexity_colours <- function() {
